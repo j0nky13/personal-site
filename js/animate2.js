cee80ce58e4f50adc8fa98956287136d28c0770f
Dev <dev@Jonathans-Mac-mini.local>
2022-02-27 21:00:38 -0500

    initial commit
    removed a couple files before commit

---
+++ js/animate2.js
@@ -0,0 +1,21 @@
+var messageArray = ["> Thank you!"];
+var textPosition = 0;
+var speed = 100;
+
+typewriter = () => {
+    document.querySelector("#message").
+        innerHTML = messageArray[0].substring(0,
+            textPosition) + "<span>\u25ae</span>";
+
+    if (textPosition++ != messageArray[0].length)
+        setTimeout(typewriter, speed);
+    
+}
+
+window.addEventListener("load", typewriter)
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+  }
